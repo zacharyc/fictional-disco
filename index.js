f505c515655429acd5ec405be4319d790b478099
Zachary Cohen <zack@ruist.com>
2021-06-12 14:12:14 -0600

    Turn it into a web server

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 
+
+const express = require('express')
+const app = express()
+const port = 3000
+
 const fs = require('fs');
 
 var file = fs.readFileSync("./prompts.txt", 'utf8');
 
 console.log(file);
+
+
 console.log('Hello, world!');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
